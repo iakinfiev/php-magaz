714650092963f456a248e7f12e77df5aed5a2508
heess <heess@mail.ru>
2020-05-22 16:27:33 +0300

    test

--- www/index.php
+++ www/index.php
@@ -1,2 +1,5 @@
 <?php
-echo('!!rt!!!');
+$controllerName  = isset($_GET['controller'])? ucfirst($_GET['controller']):'Index';
+$actionName  = isset($_GET['action'])? $_GET['action'] : 'index';
+echo 'подключенный файл controller = '. $controllerName .'</br>';
+echo 'подключенный файл action = '. $actionName .'</br>';
